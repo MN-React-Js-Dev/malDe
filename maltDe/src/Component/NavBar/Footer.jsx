00f57fe8a007fb927606dd4d6f76ea4f9af263da
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-02-02 11:09:15 +0530

    More chnages 2-2-2023

--- maltDe/src/Component/NavBar/Footer.jsx
+++ maltDe/src/Component/NavBar/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg-12">
-          <p>Copyright © 2021 ROSHAN 
+          <p>Copyright © 2022 ROSHAN 
           
           <br/>Web Designed by <a rel="nofollow" href="https://koliinfotech.com/" title="free CSS templates">Koli Infotech</a></p>
         </div>
